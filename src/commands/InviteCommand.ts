97cc2292f7cad65691f6f83106e92f07c9b878fe
Jingexz01 <64733953+Jingexz01@users.noreply.github.com>
2021-04-06 18:15:33 +0800

    Update InviteCommand.ts

--- src/commands/InviteCommand.ts
+++ src/commands/InviteCommand.ts
@@ -14,8 +14,9 @@ export class InviteCommand extends BaseCommand {
     public async execute(message: IMessage): Promise<void> {
         message.channel.send(
             createEmbed("info")
-                .addField("Discord Support Server", `[Click Here To Join](https://discord.gg/DBHxxT7)`)
+                .setTimestamp()
+                .addField("Discord Support Server", `[Click here](https://discord.gg/DBHxxT7)`)
                 .addField("Discord bot invite link", `[Click here](${await this.client.generateInvite({ permissions: 53857345 })})`)
         ).catch(e => this.client.logger.error("PLAY_CMD_ERR:", e));
-    } 
+    }
 }
