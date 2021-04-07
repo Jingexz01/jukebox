fda2c9259cb72cd942b9d33a79659166e65c955a
Jingexz01 <64733953+Jingexz01@users.noreply.github.com>
2021-04-07 09:29:51 +0800

    Update QueueCommand.ts

--- src/commands/QueueCommand.ts
+++ src/commands/QueueCommand.ts
@@ -24,26 +24,26 @@ export class QueueCommand extends BaseCommand {
         if (Number(message.guild?.queue?.songs.size) > 12) {
             const indexes: string[] = this.chunk(songs!, 12);
             let index = 0;
-            embed.setDescription(indexes[index]).setFooter(`Page ${index + 1} of ${indexes.length}`, "https://raw.githubusercontent.com/Hazmi35/jukebox/main/.github/images/info.png");
+            embed.setDescription(indexes[index]).setTimestamp().setFooter(`Page ${index + 1} of ${indexes.length}`, `Command Queue Was Requested By: ${message.author.tag}`);
             message.channel.send(embed).then(msg => {
                 msg.react("◀️").then(() => {
                     msg.react("▶️").catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
                     msg.createReactionCollector((reaction, user) => reaction.emoji.name === "◀️" && user.id === message.author.id, { time: 80 * 1000 }).on("collect", () => {
                         if (index === 0) return undefined;
                         index--;
-                        embed.setDescription(indexes[index]).setFooter(`Page ${index + 1} of ${indexes.length}`, `Command Queue Was Requested By: ${message.author.tag}`);
+                        embed.setDescription(indexes[index]).setTimestamp().setFooter(`Page ${index + 1} of ${indexes.length}`, `Command Queue Was Requested By: ${message.author.tag}`);
                         msg.edit(embed).catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
                     });
                     msg.createReactionCollector((reaction, user) => reaction.emoji.name === "▶️" && user.id === message.author.id, { time: 80 * 1000 }).on("collect", () => {
                         if (index + 1 === indexes.length) return undefined;
                         index++;
-                        embed.setDescription(indexes[index]).setFooter(`Page ${index + 1} of ${indexes.length}`, `Command Queue Was Requested By: ${message.author.tag}`);
+                        embed.setDescription(indexes[index]).setTimestamp().setFooter(`Page ${index + 1} of ${indexes.length}`, `Command Queue Was Requested By: ${message.author.tag}`);
                         msg.edit(embed).catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
                     });
                 }).catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
             }).catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
         } else {
-            message.channel.send(embed.setTimestamp().setFooter(`Command Pause Was Requested By: ${message.author.tag}`, message.author.displayAvatarURL()).setDescription(songs!.join("\n"))).catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
+            message.channel.send(embed.setTimestamp().setFooter(`Command Queue Was Requested By: ${message.author.tag}`, message.author.displayAvatarURL()).setDescription(songs!.join("\n"))).catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
         }
     }
 
