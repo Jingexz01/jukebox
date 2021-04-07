7096ff70de667cb1d066790b1deb2922cd7c690e
Jingexz01 <64733953+Jingexz01@users.noreply.github.com>
2021-04-07 17:36:07 +0800

    Update ResumeCommand.ts

--- src/commands/ResumeCommand.ts
+++ src/commands/ResumeCommand.ts
@@ -15,11 +15,11 @@ export class ResumeCommand extends BaseCommand {
     @isSameVoiceChannel()
     public execute(message: IMessage): any {
         if (message.guild?.queue?.playing) {
-            message.channel.send(createEmbed("warn", "❗ The music player is not paused!")).catch(e => this.client.logger.error("RESUME_CMD_ERR:", e));
+            message.channel.send(createEmbed("warn", "❗ The music player is not paused!").setTimestamp().setFooter(`Command Ping Was Requested by: ${message.author.tag}`, message.author.displayAvatarURL())).catch(e => this.client.logger.error("RESUME_CMD_ERR:", e));
         } else {
             message.guild!.queue!.playing = true;
             message.guild?.queue?.connection?.dispatcher.resume();
-            message.channel.send(createEmbed("info", "▶ The music player resumed")).catch(e => this.client.logger.error("RESUME_CMD_ERR:", e));
+            message.channel.send(createEmbed("info", "▶ The music player resumed").setTimestamp().setFooter(`Command Ping Was Requested by: ${message.author.tag}`, message.author.displayAvatarURL())).catch(e => this.client.logger.error("RESUME_CMD_ERR:", e));
         }
     }
 }
