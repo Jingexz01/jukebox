4488c2f47a9bef234404e0870f504495731eb1b2
Jingexz01 <64733953+Jingexz01@users.noreply.github.com>
2021-04-07 08:58:37 +0800

    Update PauseCommand.ts

--- src/commands/PauseCommand.ts
+++ src/commands/PauseCommand.ts
@@ -6,7 +6,7 @@ import { createEmbed } from "../utils/createEmbed";
 
 @DefineCommand({
     name: "pause",
-    description: "Pause the music player",
+    description: "Pause the current song.",
     usage: "{prefix}pause"
 })
 export class PauseCommand extends BaseCommand {
@@ -17,13 +17,11 @@ export class PauseCommand extends BaseCommand {
         if (message.guild?.queue?.playing) {
             message.guild.queue.playing = false;
             message.guild.queue.connection?.dispatcher.pause();
-            return message.channel.send(createEmbed("info", "⏸ The music player paused")
-                .setTimestamp()
-                .setFooter(`Command Pause Was Requested By: ${message.author.tag}`, message.author.displayAvatarURL()));
+            return message.channel.send(createEmbed("info", "⏸ Paused the music for you!"));
         }
-        message.channel.send(createEmbed("warn", "❗ The music player is already paused!")
-           .setTimestamp()
-           .setFooter(`Command NowpLaying Was Requested By: ${message.author.tag}`, message.author.displayAvatarURL()))
+        message.channel.send(createEmbed("warn", "❗ Music is already paused!")
+            .setTimestamp()
+            .setFooter(`Command NowpLaying Was Requested By: ${message.author.tag}`, message.author.displayAvatarURL()))
             .catch(e => this.client.logger.error("PAUSE_CMD_ERR:", e));
     }
 }
