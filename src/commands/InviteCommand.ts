ea8059aa535048b408d3ad6bdc24e1f076e4dbc3
Jingexz01 <64733953+Jingexz01@users.noreply.github.com>
2021-04-08 11:45:30 +0800

    Update InviteCommand.ts

--- src/commands/InviteCommand.ts
+++ src/commands/InviteCommand.ts
@@ -16,7 +16,7 @@ export class InviteCommand extends BaseCommand {
             createEmbed("info")
                 .setTimestamp()
                 .setTitle(`Jingexz01's Social Accounts`)
-                .setFooter(`Command Invite Was Requested By: ${message.author.tag}`, message.author.displayAvatarURL())
+                .setFooter(`Command Link Was Requested/\Executed By: ${message.author.tag}`, message.author.displayAvatarURL())
                 .addField("Discord Support Server", `[Click here](https://discord.gg/DBHxxT7)`)
                 .addField("Jingexz01's Youtube Account", `[Subscribe And Check Out My Youtube Channel!](https://youtube.com/jingexz01)`)
                 .addField("Jingexz01's Website", `[Open Here](https://jingexz01.cf)`)
