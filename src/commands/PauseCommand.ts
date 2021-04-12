934ebaa093e9d44f98536f802f2467f818c6592e
Jingexz01 <64733953+Jingexz01@users.noreply.github.com>
2021-04-12 13:19:03 +0800

    Update PauseCommand.ts

--- src/commands/PauseCommand.ts
+++ src/commands/PauseCommand.ts
@@ -19,11 +19,13 @@ export class PauseCommand extends BaseCommand {
             message.guild.queue.connection?.dispatcher.pause();
             return message.channel.send(createEmbed("info", "⏸ Paused the music for you!")
                 .setTimestamp()
-                .setFooter(`Command Pause Was Requested By: ${message.author.tag}`, message.author.displayAvatarURL()));
+                .setColor("#000000")
+                .setFooter(`Command Pause Was Requested/\Executed By: ${message.author.tag}`, message.author.displayAvatarURL()));
         }
         message.channel.send(createEmbed("warn", "❗ Music is already paused!")
             .setTimestamp()
-            .setFooter(`Command Pause Was Requested By: ${message.author.tag}`, message.author.displayAvatarURL()))
+            .setColor("#000000")
+            .setFooter(`Command Pause Was Requested/\Executed By: ${message.author.tag}`, message.author.displayAvatarURL()))
             .catch(e => this.client.logger.error("PAUSE_CMD_ERR:", e));
     }
 }
